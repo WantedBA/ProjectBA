2d0b036389c8aa47ed032bac5a7da3425e7df26d
one62 <onemo@kakao.com>
2026-05-08 18:10:55 +0900

    스킬 노드, 트리 헤더 작성 중
    - UserDataSubSystem과 TableManager에서 담당할 부분과 분리 필요

--- Source/BAProject/UI/SkillTree/SkillNodeWidget.h
+++ Source/BAProject/UI/SkillTree/SkillNodeWidget.h
@@ -4,7 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Instance/UserDataSubSystem.h"
 #include "SkillNodeWidget.generated.h"
+
+// 게임 중 스킬 상태
+UENUM(BlueprintType)
+enum class ESkillNodeState : uint8
+{
+	Locked		UMETA(DisplayName = "Locked"),
+	Available	UMETA(DisplayName = "Available"),
+	Learned		UMETA(DisplayName = "Learned"),
+};
 
 /**
  * 
@@ -14,8 +24,56 @@ class BAPROJECT_API USkillNodeWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+	
+// 스킬트리 연결 데이터(최초 초기화 1회)
+public:
+	// 최초 생성 시 초기화
+	void InitSkillNodeData(const FSkillData& InitData);
+	
+	FORCEINLINE [[nodiscard]] int32 GetSkillId() const
+	{
+		return SkillId;
+	}
+
+	FORCEINLINE [[nodiscard]] TArray<int32> GetPrerequisiteSkillIds() const
+	{
+		return PrerequisiteSkillIds;
+	}
+
+	FORCEINLINE [[nodiscard]] TArray<int32> GetChildSkillIds() const
+	{
+		return ChildSkillIds;
+	}
+
 protected:
-	UPROPERTY(BlueprintReadOnly)
-	int32 SkillTid;
+	// ChildSkill 설정.
+	void SetChildSkillIds(const TArray<int32>& InChildSkillIds)
+	{
+		this->ChildSkillIds = InChildSkillIds;
+	}
+	
+	UPROPERTY(BlueprintReadOnly, Category = SkillTree)
+	int32 SkillId = -1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SkillTree)
+	TArray<int32> PrerequisiteSkillIds;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SkillTree)
+	TArray<int32> ChildSkillIds;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SkillTree)
+	FSkillData SkillData;
+
+// 게임 중 상태
+public:
+	FORCEINLINE void SetSkillNodeState(const ESkillNodeState InSkillNodeState)
+	{
+		this->SkillNodeState = InSkillNodeState;
+	}
+
+protected:
+	// 게임 중 스킬 상태(SkillTree에서 설정)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SkillTree)
+	ESkillNodeState SkillNodeState = ESkillNodeState::Locked;
 	
 };
--- Source/BAProject/UI/SkillTree/SkillTreeWidget.h
+++ Source/BAProject/UI/SkillTree/SkillTreeWidget.h
@@ -15,8 +15,39 @@ class BAPROJECT_API USkillTreeWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+// 스킬트리 최초 구성
+public:
+	UFUNCTION(BlueprintCallable, Category=SkillTree)
+	void InitSkillTree();
+
 protected:
+	// 스킬 노드 생성
+	USkillNodeWidget* CreateSkillNodeWidget(const FSkillData& SkillData);
+	
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<USkillNodeWidget> SkillNodeWidgetClass;
+
+	UPROPERTY()
+	TMap<int32, TObjectPtr<USkillNodeWidget>> SkillNodeMap;
 	
+// 스킬트리 창 안에서 사용
+public:
+	// 스킬 노드 상태 갱신
+	void RefreshAllNodeStates();
+
+	// 현재 스킬 상태
+	bool IsSkillLearned(int32 SkillId);
+	bool CanLearnSkill(int32 SkillId);
+	
+	// 스킬 노드 클릭 이벤트 처리
+	void OnSkillNodeClicked(int32 SkillId);
+	
+	// 스킬 포인트 증감
+	void AddSkillPoint(int32 Amount);
+	void SpendSkillPoint(int32 Amount);
+	
+protected:
+	// 전체 스킬 포인트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=SkillTree)
+	int32 SkillPoint = 0;
 };
