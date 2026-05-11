25b2270f758765eaedfb88ce28145c56aa2f65ab
min-gyu Kwak <87803596+HiimKwak@users.noreply.github.com>
2026-05-11 16:54:16 +0900

    Refactor/player input (#7)
    * chore: Content/Character 폴더명 바꾸기
    
    * feat: 게임 시작 시 플레이어 컨트롤러로 플레이어 캐릭터 조종, 이동 설정
    
    * feat: 플레이어캐릭터 스켈레탈메시 추가

--- Source/BAProject/Player/BAPlayerCharacter.h
+++ Source/BAProject/Player/BAPlayerCharacter.h
@@ -15,13 +15,6 @@ class BAPROJECT_API ABAPlayerCharacter : public ACharacterBase
 	
 public:
 	ABAPlayerCharacter();
-	
-protected:
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
-protected:
-	void Move(const FInputActionValue& Value);
-	void Look(const FInputActionValue& Value);
 	virtual void Attack() override;
 	
 protected:
@@ -30,19 +23,6 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 	TObjectPtr<class UCameraComponent> Camera;
-
-protected:
-	UPROPERTY(EditDefaultsOnly, Category = Input, BlueprintReadOnly)
-	TObjectPtr<UInputMappingContext> InputMappingContext;
-
-	UPROPERTY(EditDefaultsOnly, Category = Input, BlueprintReadOnly)
-	TObjectPtr<UInputAction> MoveAction;
-
-	UPROPERTY(EditDefaultsOnly, Category = Input, BlueprintReadOnly)
-	TObjectPtr<UInputAction> LookAction;
-	
-	UPROPERTY(EditDefaultsOnly, Category = Input, BlueprintReadOnly)
-	TObjectPtr<UInputAction> AttackAction;
 
 protected: // TODO: 은성님과 HUD 협업, HUDInterface 구현 필요
 	// virtual	void SetupHUDWidget(class UHUDWidget* InHUDWidget) override;
