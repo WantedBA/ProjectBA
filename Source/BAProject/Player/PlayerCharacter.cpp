67b330009c684eee1d2cabf436ea68f440604f76
곽민규 <mindol0320@gmail.com>
2026-05-11 12:36:38 +0900

    feat: PlayerCharacter 클래스 기초

---
+++ Source/BAProject/Player/PlayerCharacter.cpp
@@ -0,0 +1,113 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/PlayerCharacter.h"
+
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+	GetCharacterMovement()->JumpZVelocity = 800.0f;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(
+		TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 600.0f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(
+		TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> MoveActionRef(
+		TEXT("")
+	);
+	if (MoveActionRef.Succeeded())
+	{
+		MoveAction = MoveActionRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> LookActionRef(
+		TEXT("")
+	);
+	if (LookActionRef.Succeeded())
+	{
+		LookAction = LookActionRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> AttackActionRef(
+		TEXT("")
+	);
+	if (AttackActionRef.Succeeded())
+	{
+		AttackAction = AttackActionRef.Object;
+	}
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* EnhancedInputComponent
+		= Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(
+			MoveAction,
+			ETriggerEvent::Triggered,
+			this,
+			&APlayerCharacter::Move
+		);
+
+		EnhancedInputComponent->BindAction(
+			LookAction,
+			ETriggerEvent::Triggered,
+			this,
+			&APlayerCharacter::Look
+		);
+
+		EnhancedInputComponent->BindAction(
+			AttackAction,
+			ETriggerEvent::Triggered,
+			this,
+			&APlayerCharacter::Attack
+		);
+	}
+}
+
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2D Movement = Value.Get<FVector2D>();
+
+	FRotator Rotation = GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	FVector ForwardVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	FVector RightVector	= FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardVector, Movement.Y);
+	AddMovementInput(RightVector, Movement.X);
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D RotationValue = Value.Get<FVector2D>();
+
+	AddControllerYawInput(RotationValue.X);
+	AddControllerPitchInput(RotationValue.Y);
+}
+
+void APlayerCharacter::Attack()
+{
+	// ProcessComboCommand();
+}
---
+++ Source/BAProject/Player/PlayerCharacter.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "Character/CharacterBase.h"
+#include "PlayerCharacter.generated.h"
+
+class UInputAction;
+
+UCLASS()
+class BAPROJECT_API APlayerCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+public:
+	APlayerCharacter();
+	
+protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	virtual void Attack() override;
+	
+protected:
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	TObjectPtr<class USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	TObjectPtr<class UCameraComponent> Camera;
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = Input, BlueprintReadOnly)
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(VisibleAnywhere, Category = Input, BlueprintReadOnly)
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(VisibleAnywhere, Category = Input, BlueprintReadOnly)
+	TObjectPtr<UInputAction> AttackAction;
+
+protected: // TODO: 은성님과 HUD 협업, HUDInterface 구현 필요
+	// virtual	void SetupHUDWidget(class UHUDWidget* InHUDWidget) override;
+};
