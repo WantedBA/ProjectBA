ab4c89099f67155eedfa31478987a7dd84ebdcb3
min-gyu Kwak <87803596+HiimKwak@users.noreply.github.com>
2026-05-12 16:31:50 +0900

    Feat/character locomotion (#14)
    * feat: 로코모션, 대검 공격 애니메이션 추가
    
    * feat: 마네킨 오른손 대검 프리뷰메시 추가
    
    * feat: PlayerBaseStat 수정 및 적용
    
    - MoveSpeed -> Walk/Run/Sprint 분화
    - TableManager, UserDataSubsystem 연동 정상화

--- Source/BAProject/Tables/BATableManager.h
+++ Source/BAProject/Tables/BATableManager.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PlayerRows.h"
 #include "SkillRows.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "Tables/BAPropTable.h"
@@ -26,23 +27,30 @@ public:
 
 	static UBATableManager* Get(const UObject* WorldContext);
 
+	// Player
+	FORCEINLINE const FPlayerBaseStatRow* FindPlayerBaseStat() const { return PlayerBaseStatTable.Find(1); } // 플레이어는 1명이므로 매직넘버 고정
+	FORCEINLINE const FPlayerActionDataRow* FindPlayerActionData(const int32 InTid) const { return PlayerActionDataTable.Find(InTid); }
+	
+	// Consume
 	UFUNCTION(BlueprintCallable, Category = "BA|Table")
-	bool HasConsume(int32 InTid) const { return ConsumeTable.Has(InTid); }
+	bool HasConsume(const int32 InTid) const { return ConsumeTable.Has(InTid); }
 
-	const FConsumeItemRow* FindConsume(int32 InTid) const { return ConsumeTable.Find(InTid); }
+	const FConsumeItemRow* FindConsume(const int32 InTid) const { return ConsumeTable.Find(InTid); }
 
 	UFUNCTION(BlueprintCallable, Category = "BA|Table", meta = (DisplayName = "Find Consume"))
 	bool BP_FindConsume(int32 InTid, FConsumeItemRow& OutRow) const;
 
 	const TMap<int32, FConsumeItemRow*>& GetConsumeMap() const { return ConsumeTable.GetMap(); }
 	
-	const FSkillRow* FindSkill(int32 InTid) const { return SkillTable.Find(InTid); }
+	// Skill
+	const FSkillRow* FindSkill(const int32 InTid) const { return SkillTable.Find(InTid); }
 
-	const FMonsterRows* FindMonster(int32 InTid) const { return MonsterTable.Find(InTid); }
+	// Monster
+	const FMonsterRows* FindMonster(const int32 InTid) const { return MonsterTable.Find(InTid); }
 	
-	const F1StageBossAttackRows* FindBossAttack1(int32 InTid) const { return BossAttackTable1.Find(InTid); }
-	const F2StageBossAttackRows* FindBossAttack2(int32 InTid) const { return BossAttackTable2.Find(InTid); }
-	const F3StageBossAttackRows* FindBossAttack3(int32 InTid) const { return BossAttackTable3.Find(InTid); }
+	const F1StageBossAttackRows* FindBossAttack1(const int32 InTid) const { return BossAttackTable1.Find(InTid); }
+	const F2StageBossAttackRows* FindBossAttack2(const int32 InTid) const { return BossAttackTable2.Find(InTid); }
+	const F3StageBossAttackRows* FindBossAttack3(const int32 InTid) const { return BossAttackTable3.Find(InTid); }
 
 	const TMap<int32, F1StageBossAttackRows*>& GetBossAttackMap1() const { return BossAttackTable1.GetMap(); }
 	const TMap<int32, F2StageBossAttackRows*>& GetBossAttackMap2() const { return BossAttackTable2.GetMap(); }
@@ -52,6 +60,9 @@ private:
 	template<typename RowType, typename KeyType>
 	void LoadTable(TBAPropTable<RowType, KeyType>& OutTable, const FString AssetPath);
 
+	TBAPropTable<FPlayerBaseStatRow, int32> PlayerBaseStatTable;
+	TBAPropTable<FPlayerActionDataRow, int32> PlayerActionDataTable;
+	
 	TBAPropTable<FConsumeItemRow, int32> ConsumeTable;
 	
 	TBAPropTable<FSkillRow, int32> SkillTable;
