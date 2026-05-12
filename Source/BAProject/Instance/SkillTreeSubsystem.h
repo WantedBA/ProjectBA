a0db278a219766f88041dbc1ed88a5566ac8752b
one62 <onemo@kakao.com>
2026-05-12 12:18:44 +0900

    스킬 관련 내용 UserDataSubsystem -> SkillTreeSubsystem 이동

--- Source/BAProject/Instance/SkillTreeSubsystem.h
+++ Source/BAProject/Instance/SkillTreeSubsystem.h
@@ -6,6 +6,22 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "SkillTreeSubsystem.generated.h"
 
+// Todo: 임의 작업 후에 SkillData 관련 구조체 제대로 설정되면 변경 요망.
+USTRUCT(BlueprintType)
+struct FSkillData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadOnly)
+	int32 SkillTid;
+	UPROPERTY(BlueprintReadOnly)
+	int32 SkillCost = 1; // 스킬 획득 비용(스킬포인트)
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSkillPointsChanged, int32, NewSkillPoints);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAddSkillData, FSkillData, SkillData);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnRemoveSkillData, int32, SkillTid);
+
 /**
  * 
  */
@@ -15,10 +31,52 @@ class BAPROJECT_API USkillTreeSubsystem : public UGameInstanceSubsystem
 	GENERATED_BODY()
 	
 public:
+	USkillTreeSubsystem();
+	
+	// 재정의 함수
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	
+	// 전역 접근
+	static USkillTreeSubsystem* Get(const UObject* WorldContext);
+	
+	// Getter
+	UFUNCTION(BlueprintPure)
+	int32 GetSkillPoints() const { return SkillPoints; }
 	
 private:
+	// UserData 참조
 	UPROPERTY()
 	class UUserDataSubSystem* UserData;
 	
+	// 스킬트리 관련
+	//SkillPoint
+	UFUNCTION(BlueprintCallable)
+	void AddSkillPoints(int32 Amount);
+
+	//AddSkill
+	void AddSkillData(FSkillData data);
+	void AddSkillData(int32 skillTid);
+	
+	//RemoveSkill
+	void RemoveSkillData(int32 skillTid);
+	
+		
+	//Delegate
+	UPROPERTY(BlueprintAssignable)
+	FOnSkillPointsChanged OnSkillPointsChanged;
+	UPROPERTY(BlueprintAssignable)
+	FOnAddSkillData OnAddSkillData;
+	UPROPERTY(BlueprintAssignable)
+	FOnRemoveSkillData OnRemoveSkillData;
+	
+	const FSkillData* FindSkillData(int32 skillTid) const;
+	
+	
+protected:
+	UPROPERTY(BlueprintReadWrite)
+	int32 SkillPoints = 0;
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FSkillData> OwnedSkillDatas;
 };
--- Source/BAProject/Instance/UserDataSubSystem.h
+++ Source/BAProject/Instance/UserDataSubSystem.h
@@ -22,22 +22,6 @@ struct FPlayerBaseStat
 	int32 MaxStamina = 0; // 테이블에서 정의한 Stamina 최대치
 };
 
-// Todo: 임의 작업 후에 SkillData 관련 구조체 제대로 설정되면 변경 요망.
-USTRUCT(BlueprintType)
-struct FSkillData
-{
-	GENERATED_BODY()
-	
-	UPROPERTY(BlueprintReadOnly)
-	int32 SkillTid;
-	UPROPERTY(BlueprintReadOnly)
-	int32 SkillCost = 1; // 스킬 획득 비용(스킬포인트)
-};
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSkillPointsChanged, int32, NewSkillPoints);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAddSkillData, FSkillData, SkillData);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnRemoveSkillData, int32, SkillTid);
-
 UCLASS()
 class BAPROJECT_API UUserDataSubSystem : public UGameInstanceSubsystem
 {
@@ -54,44 +38,15 @@ public:
 	//BaseStat 기본값
 	void SetBaseStat();
 	
-// 스킬트리 관련
-	//SkillPoint
-	UFUNCTION(BlueprintCallable)
-	void AddSkillPoints(int32 Amount);
-
-	//AddSkill
-	void AddSkillData(FSkillData data);
-	void AddSkillData(int32 skillTid);
-	
-	//RemoveSkill
-	void RemoveSkillData(int32 skillTid);
-	
 	//Getter
 	UFUNCTION(BlueprintPure)
 	int32 GetCurMaxStamina() const { return CurMaxStamina; }
-	UFUNCTION(BlueprintPure)
-	int32 GetSkillPoints() const { return SkillPoints; }
-	
-	//Delegate
-	UPROPERTY(BlueprintAssignable)
-	FOnSkillPointsChanged OnSkillPointsChanged;
-	UPROPERTY(BlueprintAssignable)
-	FOnAddSkillData OnAddSkillData;
-	UPROPERTY(BlueprintAssignable)
-	FOnRemoveSkillData OnRemoveSkillData;
-	
-	const FSkillData* FindSkillData(int32 skillTid) const;
-	
+
 protected:
 	UPROPERTY(BlueprintReadOnly)
 	FPlayerBaseStat BaseStat;
 	UPROPERTY(BlueprintReadWrite)
-	int32 SkillPoints = 0;
-	UPROPERTY(BlueprintReadWrite)
 	int32 CurMaxHp = 0; // 스킬에 따라 더 늘어날 수 있음
 	UPROPERTY(BlueprintReadWrite)
 	int32 CurMaxStamina = 0; // 스킬에 따라 더 늘어날 수 있음
-	UPROPERTY(BlueprintReadOnly)
-	TArray<FSkillData> OwnedSkillDatas;
-	
 };
