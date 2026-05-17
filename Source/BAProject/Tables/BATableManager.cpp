0ff5cdb0a1a26c491a9fec1235b280199ebbd6d3
JinHyeok <kksd2950@gmail.com>
2026-05-18 00:12:43 +0900

    Quest Base 작업 진행
    -QuestManagerSubsystem 작업
    -Quest관련 테이블 연동
    -Enemy에 Delegate 작업 추가

--- Source/BAProject/Tables/BATableManager.cpp
+++ Source/BAProject/Tables/BATableManager.cpp
@@ -58,6 +58,13 @@ void UBATableManager::LoadAllTables()
 	LoadTable(BossAttackTable2, *(TablePath + TEXT("DT_BossMonster_2StageBossAttack.DT_BossMonster_2StageBossAttack")));
 	LoadTable(BossAttackTable3, *(TablePath + TEXT("DT_BossMonster_3StageBossAttack.DT_BossMonster_3StageBossAttack")));
 
+	// Quest
+	LoadTable(QuestTable,      *(TablePath + TEXT("DT_Quest_Quest.DT_Quest_Quest")));
+	LoadTable(ZoneMonsterTable, *(TablePath + TEXT("DT_Quest_ZoneMonster.DT_Quest_ZoneMonster")));
+
+	// Reward
+	LoadTable(RewardTable, *(TablePath + TEXT("DT_Reward_Reward.DT_Reward_Reward")));
+
 	for (IBAPostRead* Table : PostReadList)
 	{
 		Table->PostRead();
@@ -102,6 +109,32 @@ void UBATableManager::LoadTable(TBAPropTable<RowType, KeyType>& OutTable, const 
 	LoadedTables.Add(DataTable);
 	OutTable.Build(DataTable);
 	PostReadList.Add(&OutTable);
+}
+
+TArray<const FZoneMonsterRows*> UBATableManager::GetZoneMonstersByQuest(int32 InQuestTid) const
+{
+	TArray<const FZoneMonsterRows*> Result;
+	for (const TPair<int32, FZoneMonsterRows*>& Pair : ZoneMonsterTable.GetMap())
+	{
+		if (Pair.Value && Pair.Value->QuestTid == InQuestTid)
+		{
+			Result.Add(Pair.Value);
+		}
+	}
+	return Result;
+}
+
+TArray<const FRewardRows*> UBATableManager::GetRewardsByTid(int32 InRewardTid) const
+{
+	TArray<const FRewardRows*> Result;
+	for (const TPair<int32, FRewardRows*>& Pair : RewardTable.GetMap())
+	{
+		if (Pair.Value && Pair.Value->RewardTid == InRewardTid)
+		{
+			Result.Add(Pair.Value);
+		}
+	}
+	return Result;
 }
 
 void UBATableManager::BuildChildSkillLists()
