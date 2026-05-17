6043063c624a7b3e7d3ae97022600fddb9301860
FW <62225970+GyumyeongShim@users.noreply.github.com>
2026-05-18 00:37:39 +0900

    코드 수정
    -Hit 처리
    -디버그 추가
    -충격파
    -역경직 추가

--- Source/BAProject/Component/CombatComponent.h
+++ Source/BAProject/Component/CombatComponent.h
@@ -6,6 +6,7 @@
 
 class UAnimMontage;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHitDetected, AActor*, Victim, const FHitResult&, HitResult);
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class BAPROJECT_API UCombatComponent : public UActorComponent
 {
@@ -14,26 +15,46 @@ class BAPROJECT_API UCombatComponent : public UActorComponent
 public:
 	UCombatComponent();
 
-	UFUNCTION(BlueprintCallable, Category = "Combat") // 몽타주 사용
+	UFUNCTION(BlueprintCallable, Category = "Combat")
 	void ExecuteAttack(UAnimMontage* AttackMontage, float PlayRate = 1.0f);
 
 	UFUNCTION(BlueprintCallable, Category = "Combat")
-	void CheckAnimNotifyHitStart(float InRadius, float InDamage, FName InSocketName = TEXT("Muzzle"));
+	void CheckHitStart(float InRadius, float InDamage, FName InSocketName = TEXT("Muzzle"));
 
 	UFUNCTION(BlueprintCallable, Category = "Combat")
-	void CheckAnimNotifyHitEnd();
+	void CheckHitEnd();
 
 	UFUNCTION(BlueprintCallable, Category = "Combat")
-	void SetWeaponCollisionActive(bool bActive);
+	void SetAttackData(float InRadius, float InDamage, FName InSocketName);
+
+	void TriggerHitStop(float Duration);
+
+	UFUNCTION(BlueprintCallable, Category = "Combat|VFX")
+	void SpawnShockwave(FVector Location, float Scale = 1.0f);
 
 protected:
-	void CheckHit();
-
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	void ProcessHitCheck(); // 이전 프레임 소켓 위치와 현재 위치 사이를 스윕
+
+	void ApplyDamage(AActor* Victim, const FHitResult& HitResult);
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnHitDetected OnHitDetected;
+
 protected:
+	UPROPERTY(EditAnywhere, Category = "Combat|VFX")
+	TObjectPtr<class UNiagaraSystem> ShockwaveSystem;
+
+	UPROPERTY(EditAnywhere, Category = "Combat|VFX")
+	TObjectPtr<class UNiagaraSystem> DistortionSystem;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat|Debug")
+	bool bShowDebugTrace = false;
+
 	UPROPERTY()
-	bool bIsHitChecking;
+	bool bIsHitChecking = false;
 
 	UPROPERTY()
 	float CurrentRadius;
@@ -45,5 +66,11 @@ protected:
 	FName CurrentSocketName;
 
 	UPROPERTY()
-	TArray<AActor*> HitActors;
+	FVector PrevSocketLocation;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> HitActors;
+
+	FTimerHandle HitStopTimerHandle;
+	bool bIsHitStopActive = false;
 };
--- Source/BAProject/Enemy/Animation/ANS_Collision.cpp
+++ Source/BAProject/Enemy/Animation/ANS_Collision.cpp
@@ -20,7 +20,7 @@ void UANS_Collision::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequence
 	UCombatComponent* CombatComp = Enemy->GetComponentByClass<UCombatComponent>();
 	if (CombatComp)
 	{
-		CombatComp->SetWeaponCollisionActive(true);
+		CombatComp->CheckHitStart();
 	}
 }
 
@@ -42,6 +42,6 @@ void UANS_Collision::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBa
 	UCombatComponent* CombatComp = Enemy->GetComponentByClass<UCombatComponent>();
 	if (CombatComp)
 	{
-		CombatComp->SetWeaponCollisionActive(false);
+		CombatComp->CheckHitEnd();
 	}
 }
