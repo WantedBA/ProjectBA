13ccebc348c7907293dc92d2a2b16136bd5c03e4
ShimGyuMyeong <sgm8626@gmail.com>
2026-05-18 15:44:37 +0900

    코드 수정
    - CheckState BT노드에서 설정하도록 추가

--- Source/BAProject/Enemy/AI/BTDecorator_CheckState.cpp
+++ Source/BAProject/Enemy/AI/BTDecorator_CheckState.cpp
@@ -1,9 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Enemy/AI/BTDecorator_CheckState.h"
+#include "AIController.h"
+
+UBTDecorator_CheckState::UBTDecorator_CheckState()
+{
+    NodeName = TEXT("CheckState");
+}
 
 bool UBTDecorator_CheckState::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
 {
-	return false;
+    AEnemyBase* Enemy = Cast<AEnemyBase>(OwnerComp.GetAIOwner()->GetPawn());
+    if (Enemy == nullptr)
+    {
+        return false;
+    }
+
+    return Enemy->GetCurrentState() == CheckState;
 }
--- Source/BAProject/Enemy/AI/BTDecorator_CheckState.h
+++ Source/BAProject/Enemy/AI/BTDecorator_CheckState.h
@@ -1,19 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTDecorator.h"
+#include "Enemy/EnemyBase.h"
 #include "BTDecorator_CheckState.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class BAPROJECT_API UBTDecorator_CheckState : public UBTDecorator
 {
 	GENERATED_BODY()
+
+public:
+	UBTDecorator_CheckState();
 	
 protected:
 	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Condition")
+	EEnemyState CheckState;
 };
