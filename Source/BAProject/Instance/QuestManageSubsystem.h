8766ab973afe463c08789e1fbad7534747244753
ShimGyuMyeong <sgm8626@gmail.com>
2026-05-20 19:57:13 +0900

    코드 수정
    -몬스터 destroy
    -재생성 처리

--- Source/BAProject/Instance/QuestManageSubsystem.h
+++ Source/BAProject/Instance/QuestManageSubsystem.h
@@ -39,10 +39,11 @@ public:
     void RegisterTrigger(int32 QuestTid, AQuestZoneActor* Trigger);
     void AbortQuest(int32 QuestTid);
 
-    void RefreshQuestMonsters(int32 QuestTid, const TArray<FTransform>& SpawnTransforms, TSubclassOf<AMonster> MonsterClass);
-    void RespawnQuestZoneEnemies();
-    
+    void ClearQuestMonsters(int32 QuestTid); // 모든 몬스터 제거
+    void RefreshQuestMonsters(int32 QuestTid, const TArray<FTransform>& SpawnTransforms, TSubclassOf<AMonster> MonsterClass); // 몬스터 재 생성
+    void RespawnQuestZoneEnemies(); // 퀘스트존 순회하면서 리스폰, 추후 지역Tid 생기면 해당 값에 따라 건너뜀처리 해야함
     bool IsQuestCompleted(int32 QuestTid) const { return CompletedQuests.Contains(QuestTid); }
+
     TSet<int32> MakeQuestSaveData() const { return CompletedQuests; }
     void ApplyQuestSaveData(const TSet<int32>& QuestSaveDataTSet);
 
@@ -57,10 +58,12 @@ private:
     void SetActivatablesActive(int32 QuestTid, bool bActive);
 
     TMap<int32, FQuestRuntimeState> ActiveQuests;
+    TSet<int32> CompletedQuests;
+
+    TMap<int32, TArray<TWeakObjectPtr<AActor>>> AllSpawnedMonsters; // 퀘스트별로 소환된 모든 몬스터, 리셋시 청소용
+
     TMap<int32, TArray<TWeakObjectPtr<AActor>>> PendingMonsters;
     TMap<int32, TArray<TWeakObjectPtr<AActor>>> PendingActivatables;
     
     TMap<int32, TArray<TWeakObjectPtr<AQuestZoneActor>>> RegisteredTriggers;
-
-    TSet<int32> CompletedQuests;
 };
