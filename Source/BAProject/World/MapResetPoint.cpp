96d7a44ede497561b47aa53f3f219fd142273523
ShimGyuMyeong <sgm8626@gmail.com>
2026-05-20 16:27:50 +0900

    코드 추가
    - 체크포인트 추가

---
+++ Source/BAProject/World/MapResetPoint.cpp
@@ -0,0 +1,94 @@
+#include "World/MapResetPoint.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Player/BAPlayerCharacter.h"
+#include "Component/StatComponent.h"
+#include "Instance/QuestManageSubsystem.h"
+#include "SaveGame/SaveGameManager.h"
+#include "Kismet/GameplayStatics.h"
+
+AMapResetPoint::AMapResetPoint()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	InteractionPivot = CreateDefaultSubobject<USceneComponent>(TEXT("InteractionPivot"));
+	RootComponent = InteractionPivot;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(RootComponent);
+	MeshComponent->SetCollisionProfileName(TEXT("NoCollision"));
+}
+
+void AMapResetPoint::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+bool AMapResetPoint::CanInteract_Implementation(AActor* Interactor) const
+{
+	if (bConditionUnlocked == false)
+	{
+		return false;
+	}
+	
+	return true;
+}
+
+void AMapResetPoint::Interact_Implementation(AActor* Interactor)
+{
+	ABAPlayerCharacter* Player = Cast<ABAPlayerCharacter>(Interactor);
+	if (Player == nullptr)
+	{
+		return;
+	}
+
+	if (bIsPlayerResting == true)
+	{
+		bIsPlayerResting = false;
+		Player->UnlockMovementForCutscene();
+	}
+	else
+	{
+		bIsPlayerResting = true;
+
+		// 1. RecoverPlayer
+		if (UStatComponent* StatComp = Player->FindComponentByClass<UStatComponent>())
+		{
+			StatComp->RestoreAll();
+		}
+		Player->ResetStatusForMapResetPoint();
+
+		// 2. RefillConsumables
+		Player->RefillConsumables();
+
+		// 3. RespawnEnemies
+		if (UQuestManageSubsystem* QM = UQuestManageSubsystem::Get(this))
+		{
+			QM->RespawnQuestZoneEnemies();
+		}
+
+		// 4. OpenSkillTree
+		OnRequestOpenSkillTree.Broadcast();
+
+		// 5. SaveProgress
+		if (UGameInstance* GI = GetGameInstance())
+		{
+			if (USaveGameManager* SaveManager = GI->GetSubsystem<USaveGameManager>())
+			{
+				SaveManager->SaveGame();
+			}
+		}
+
+		Player->LockMovementForCutscene();
+	}
+}
+
+FText AMapResetPoint::GetInteractionPrompt_Implementation() const
+{
+	return bIsPlayerResting ? PromptExit : PromptEnter;
+}
+
+FVector AMapResetPoint::GetInteractionLocation_Implementation() const
+{
+	return GetActorLocation();
+}
