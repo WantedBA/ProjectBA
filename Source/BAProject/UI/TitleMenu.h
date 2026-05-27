6fc45cd9d23c6e4814a18803677ca1456f07f3d3
곽민규 <mindol0320@gmail.com>
2026-05-27 11:43:23 +0900

    버그 수정: 타이틀 메뉴 게임패드 스틱 탐색 복구

--- Source/BAProject/UI/TitleMenu.h
+++ Source/BAProject/UI/TitleMenu.h
@@ -3,8 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TimerManager.h"
 #include "UI/System/PopupBase.h"
 #include "TitleMenu.generated.h"
+
+enum class EUINavigation : uint8;
 
 /**
  * 
@@ -16,4 +19,33 @@ class BAPROJECT_API UTitleMenu : public UPopupBase
 	
 protected:
 	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	virtual FReply NativeOnAnalogValueChanged(const FGeometry& InGeometry, const FAnalogInputEvent& InAnalogEvent) override;
+	virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
+	virtual FReply NativeOnPreviewKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
+	virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+
+private:
+	void FocusInitialTitleButton();
+	bool TryNavigateTitleButton(EUINavigation Direction);
+	bool SelectTitleButton(class UButton* Button);
+	bool ActivateSelectedTitleButton();
+	void FinishGamepadTitleButtonClick();
+	void ClearGamepadSelectedTitleButton();
+
+	TArray<class UButton*> GetNavigableTitleButtons() const;
+	class UButton* ResolveCurrentTitleButton() const;
+	class UButton* FindBestTitleButtonInDirection(class UButton* SourceButton, EUINavigation Direction) const;
+	FVector2D GetTitleButtonCenterAbsolute(const class UButton* Button) const;
+	void MoveMouseToTitleButton(const class UButton* Button);
+	bool IsRightStickNavigationKey(const FKey& Key) const;
+	EUINavigation GetRightStickNavigationDirection() const;
+
+	TWeakObjectPtr<class UButton> GamepadSelectedButton;
+	FVector2D RightStickNavigationInput = FVector2D::ZeroVector;
+	FVector2D LastGamepadCursorAbsolute = FVector2D::ZeroVector;
+	bool bRightStickNavigationReady = true;
+	bool bGamepadNavigationActive = false;
+	bool bGamepadClickPending = false;
+	FTimerHandle GamepadClickTimerHandle;
 };
